03f6544244daedde253b4fb9ac36a6e34f6697b7
Aponia <ap0nia.dev@gmail.com>
2024-12-06 12:42:01 -0800

    feat: initial commit for sixel progress

--- MacOS/.config/nvim/init.lua
+++ MacOS/.config/nvim/init.lua
@@ -1,6 +1,46 @@
+local stdout = vim.loop.new_tty(1, false)
+local stdout = vim.loop.new_tty(1, false)
+
+local echoraw = function(str)
+    vim.fn.chansend(vim.v.stderr, str)
+end
+
 local function init()
   require('config').setup()
   require('lazy').setup('plugins')
+
+  vim.api.nvim_create_user_command("SixView", function(opts)
+    local path = '/Users/aponia/dotfiles/Linux/.config/fastfetch/logos/elysia-1.sixel'
+
+    -- Read raw Sixel data
+    local file = io.open(path, "rb")
+
+    if not file then
+        vim.api.nvim_err_writeln("Failed to open file: " .. path)
+        return
+    end
+
+    local data = file:read("*a") -- Read entire file as binary
+
+    file:close()
+
+    -- Save cursor position
+    echoraw("\27[s")
+
+    local win_position = vim.api.nvim_win_get_position(0)
+
+    local y = win_position[1]
+    local x = win_position[2]
+
+    -- Move cursor to the desired position
+    echoraw(string.format("\27[%d;%dH", y, x + 1))
+
+    -- Write the Sixel data
+    echoraw(data)
+
+    -- Restore cursor position
+    echoraw("\27[u")
+  end, {})
 end
 
 init()
---
+++ MacOS/.config/nvim/lua/plugins/pets.lua
@@ -0,0 +1,24 @@
+return {
+  "ap0nia/pets.nvim",
+
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "edluffy/hologram.nvim"
+  },
+
+  config = function()
+    require('pets').setup({
+      popup = {
+        -- popup options, try changing these if you see a rectangle around the pets
+        -- width = "10%", -- can be a string with percentage like "45%" or a number of columns like 45
+        winblend = 100, -- winblend value - see :h 'winblend' - only used if avoid_statusline is false
+        hl = {
+          Normal = "Normal"
+        }, -- hl is only set if avoid_statusline is true, you can put any hl group instead of "Normal"
+        avoid_statusline = true, 
+        -- if winblend is 100 then the popup is invisible and covers the statusline, if that
+        -- doesn't work for you then set this to true and the popup will use hl and will be spawned above the statusline (hopefully)
+      }
+    })
+  end
+}
